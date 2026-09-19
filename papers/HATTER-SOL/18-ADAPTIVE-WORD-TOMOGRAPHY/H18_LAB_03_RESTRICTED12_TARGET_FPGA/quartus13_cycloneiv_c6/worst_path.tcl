f9e858269c40564e80df470033e4229b1fbfa24d
AlexMalachevsky <t89257409519@gmail.com>
2026-09-19 20:00:11 +0300

    H18-LAB-03: add target FPGA project files

---
+++ papers/HATTER-SOL/18-ADAPTIVE-WORD-TOMOGRAPHY/H18_LAB_03_RESTRICTED12_TARGET_FPGA/quartus13_cycloneiv_c6/worst_path.tcl
@@ -0,0 +1,11 @@
+package require ::quartus::project
+package require ::quartus::sta
+
+project_open h18_r12_c4c6
+create_timing_netlist
+read_sdc
+set_operating_conditions -model slow -temperature 85 -voltage 1200
+update_timing_netlist
+report_timing -setup -from_clock clk -to_clock clk -npaths 3 -detail full_path -show_routing -file worst_path_full.rpt
+delete_timing_netlist
+project_close
